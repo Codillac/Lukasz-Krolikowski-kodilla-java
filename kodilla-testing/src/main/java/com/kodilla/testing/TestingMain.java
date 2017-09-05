1d91313f6137d0ea4f8857ad1c40421a21e62c35
“Codillac <luk.krolikowski@gmail.com>
2017-09-05 20:56:39 +0200

    Add Module 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import java.util.Random;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if(result.equals("theForumUser")) {
+            System.out.println("Forum user test OK");
+        } else {
+            System.out.println("Forum user test failed");
+        }
+
+        // Test of class Calculator methods:
+        Calculator calculator = new Calculator();
+        Random randomizer = new Random();
+        int a = randomizer.nextInt(1000000000) + 1;
+        int b = randomizer.nextInt(1000000000) + 1;
+
+        int addingResult = a + b;
+        int subtractingResult = a - b;
+
+        if(calculator.add(a, b) == addingResult){
+            System.out.println("Adding test OK");
+        } else {
+            System.out.println("Adding test FAILED!");
+        }
+
+        if(calculator.subtract(a, b) == subtractingResult){
+            System.out.println("Subtracting test OK");
+        } else {
+            System.out.println("Subtracting test FAILED!");
+        }
     }
 }
