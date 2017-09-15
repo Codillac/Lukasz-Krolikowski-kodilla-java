8e97ca8d905b65f36a202e1d4d2f912ee8602191
“Codillac <luk.krolikowski@gmail.com>
2017-09-15 19:49:11 +0200

    Add module 7.3 EXAMPLES

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,35 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("So this is how liberty dies. With the thunderous applause.", text -> text.toUpperCase());
-        poemBeautifier.beautify("So this is how liberty dies. With the thunderous applause.", text -> {
-            String resultText = "";
-            for(int i = 0; i < text.length(); i++){
-                String currentSign = "";
-                currentSign += text.charAt(i);
-                if(i % 2 == 0){
-                    currentSign = currentSign.toUpperCase();
-                }
-                resultText += currentSign;
-            }
-            return resultText;
-        });
-        poemBeautifier.beautify("Beam me up, Scotty", text -> text.replace(' ', '~'));
-        poemBeautifier.beautify("Beam me up, Scotty", text -> {
-            String resultText = "";
-            for(int i = 0; i < text.length(); i++){
+        BookDirectory theBookDirectory = new BookDirectory();
 
-                if(text.charAt(i) == ' ') {
-                    resultText += " *** ";
-                } else {
-                    resultText += text.charAt(i);
-                }
-            }
-            return resultText;
-        });
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));
+        System.out.println(theResultStringOfBooks);
     }
 }
