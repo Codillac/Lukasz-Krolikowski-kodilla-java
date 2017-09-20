26a3de8abac0aa1612c85cda7dbd992b297ed39e
“Codillac <luk.krolikowski@gmail.com>
2017-09-20 21:16:21 +0200

    Module 8.2 EXAMPLE

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+
+public class ExceptionModuleRunner {
+    public static void main(String[] args) throws Exception{
+
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+
+    }
+}
