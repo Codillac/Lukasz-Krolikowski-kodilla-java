2d05bf839e1923da6be0321903d7e06d77ef124f
“Codillac <luk.krolikowski@gmail.com>
2017-10-09 20:53:19 +0200

    Task 10.2 REFACTORED

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -2,13 +2,16 @@ package com.kodilla.spring.calculator;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Random;
 
-@SpringBootApplication
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorTestSuite {
     @Test
     public void testCalculatorAdd() {
