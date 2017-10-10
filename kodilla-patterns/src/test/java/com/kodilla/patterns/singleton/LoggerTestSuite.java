160046c3d62593713934927ea72d2d5ceb3eedc2
“Codillac <luk.krolikowski@gmail.com>
2017-10-10 19:51:28 +0200

    TASK 11.1 AFTER REQUIRED MODIFICATION!

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -7,17 +7,15 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class LoggerTestSuite {
-    private static Logger logger = new Logger();
-
     @Test
     public void loggerTest() {
         //Given
         String dateAndTimeOfLogEntry = LocalDate.now() + " " + LocalTime.now();
 
         //When
-        logger.log("Add file. " + dateAndTimeOfLogEntry);
+        Logger.getInstance().log("Add file. " + dateAndTimeOfLogEntry);
 
         //Then
-        Assert.assertEquals("Add file. " + dateAndTimeOfLogEntry, logger.getLastLog());
+        Assert.assertEquals("Add file. " + dateAndTimeOfLogEntry, Logger.getInstance().getLastLog());
     }
 }
