450118377fb32c4fa2852a325f9f09bae10a1e1b
“Codillac <luk.krolikowski@gmail.com>
2017-10-12 20:29:46 +0200

    Pizza EXAMPLE

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
@@ -7,11 +7,19 @@ public class PizzaTestSuite {
     @Test
     public void testPizzaNew() {
         //Given
-        Pizza pizza = new Pizza("Thin", "Spicy", "Mushrooms", "Onion", "Ham");
+        Pizza pizza = new Pizza.PizzaBuilder()
+                .ingredient("Onion")
+                .bottom("Thin")
+                .sauce("Spicy")
+                .ingredient("Ham")
+                .ingredient("Mushrooms")
+                .build();
         System.out.println(pizza);
 
         //When
         int howManyIngredients = pizza.getIngredients().size();
+
+        //Then
         Assert.assertEquals(3, howManyIngredients);
     }
 }
