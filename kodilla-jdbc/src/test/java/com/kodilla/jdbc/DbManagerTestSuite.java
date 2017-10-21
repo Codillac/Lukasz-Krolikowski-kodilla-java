75a00402ff5ec4c20693a5dc6fb77282aa868275
“Codillac <luk.krolikowski@gmail.com>
2017-10-21 19:42:28 +0200

    Task 12.6: Active Users

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.jdbc;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,5 +40,39 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery =   "SELECT u.*, count(*) AS POST_QTY " +
+                "FROM users u, posts p " +
+                "WHERE u.ID = p.USER_ID " +
+                "GROUP BY u.ID " +
+                "HAVING count(*) >= 2 " +
+                "ORDER BY u.ID;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            int numberOfPosts = rs.getInt("POST_QTY");
+            Assert.assertTrue(numberOfPosts >= 2);
+
+            System.out.println(rs.getInt("ID") + " " + rs.getString("FIRSTNAME") + " " + rs.getString("LASTNAME") + ", " + rs.getInt("POST_QTY") + " posts");
+
+            counter++;
+        }
+
+        Assert.assertEquals(2, counter);
+    }
+
+    @After
+    public void newLine() {
+        System.out.println();
     }
 }
