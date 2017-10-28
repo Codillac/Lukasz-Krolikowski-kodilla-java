053fe107083818aff3c0ea8b54f0a86b14bdb20c
“Codillac <luk.krolikowski@gmail.com>
2017-10-28 09:39:19 +0200

    Task 13.4: NAMED QUERIES and HQL

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,11 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.retrieveByFirstThreeLettersOfCompanyName",
+        query = "SELECT * FROM COMPANIES WHERE COMPANY_NAME LIKE CONCAT(SUBSTRING(:THREEFIRSTLETTERS FROM 1 FOR 3), '%')",
+        resultClass = Company.class
+)
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
