862b9dcf2d852b5ce83c44f233d34bc06193d0d6
“Codillac <luk.krolikowski@gmail.com>
2017-12-23 10:05:58 +0100

    Submodule 20.4: OBSERVER PATTERN

---
+++ kodilla-patterns2/src/main/java/com/codilla/patterns2/observer/forum/JavaToolsForumTobic.java
@@ -0,0 +1,7 @@
+package com.codilla.patterns2.observer.forum;
+
+public class JavaToolsForumTobic extends ForumTopic {
+    public JavaToolsForumTobic() {
+        super("Java Tools Group");
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/codilla/patterns2/observer/forum/Observer.java
@@ -0,0 +1,5 @@
+package com.codilla.patterns2.observer.forum;
+
+public interface Observer {
+    void update(ForumTopic forumTopic);
+}
